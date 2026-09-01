4b44805b22738b80e2dffbe988b9882994471b42
elbarrakhamza <elbarrakhamza@gmail.com>
2026-09-01 19:45:26 +0100

    fix: embedder dim 2048

--- vector_database/db-init/01-schema.sql
+++ vector_database/db-init/01-schema.sql
@@ -1,11 +1,10 @@
-
 CREATE EXTENSION IF NOT EXISTS vector;
 
 CREATE TABLE IF NOT EXISTS documents (
     id        SERIAL PRIMARY KEY,
     content   TEXT,
     metadata  JSONB,
-    embedding vector(1024)
+    embedding vector(2048)  -- ← CHANGÉ de 1024 à 2048
 );
 
 CREATE INDEX hnsw_index
