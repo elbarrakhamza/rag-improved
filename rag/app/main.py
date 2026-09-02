59aed9b6a209aa0b55fd265739fa858b9638adb2
elbarrakhamza <elbarrakhamza@gmail.com>
2026-09-02 22:30:08 +0100

    fixe cors main

--- rag/app/main.py
+++ rag/app/main.py
@@ -83,6 +83,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost",
         "http://localhost:4200",
         "http://localhost:3000",
         "https://rag-web.stage.enset.top",
